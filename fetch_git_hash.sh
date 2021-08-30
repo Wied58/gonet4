9169bc81814150a6583a3b16d45fdde9654db3cc
Jeff Wiedemann <fej158@gmail>
2021-08-30 13:33:03 +0100

    fixed directory for git log in fetch_git_hash.sh

--- fetch_git_hash.sh
+++ fetch_git_hash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/pi/Tools/git_repo
+cd /home/pi/Tools/Camera
 VER=`git log --pretty=oneline | head -1 | awk '{print substr($1,1,5)}'`
 rm -f /home/pi/Tools/Version/*
 touch /home/pi/Tools/Version/$VER
